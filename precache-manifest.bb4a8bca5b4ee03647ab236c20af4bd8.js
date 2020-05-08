cc67c0578c9cc8a5e5abc185324aec852b1d331f
Lazar Furtula <Lazar996F@users.noreply.github.com>
2020-05-08 22:46:05 +0200

    Updates

---
+++ precache-manifest.bb4a8bca5b4ee03647ab236c20af4bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962655eef425efc1d0bbf675e4ac43e0",
+    "url": "/cmachine/index.html"
+  },
+  {
+    "revision": "4f01d7b432ef63f9f21a",
+    "url": "/cmachine/static/css/main.74e4b675.chunk.css"
+  },
+  {
+    "revision": "b3960fecd0a6e6b138a6",
+    "url": "/cmachine/static/js/2.0d37028d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cmachine/static/js/2.0d37028d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f01d7b432ef63f9f21a",
+    "url": "/cmachine/static/js/main.0f7026fe.chunk.js"
+  },
+  {
+    "revision": "19181dc09a87758ae4b2",
+    "url": "/cmachine/static/js/runtime-main.123611df.js"
+  }
+]);
